58fe6c160150cb2fc387573aed94041b5518baa7
Leandro <repositorio.codeflow@gmail.com>
2023-06-04 18:33:42 -0300

    Aulas #7 #8 e #9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'Leandro';
+  userData = {
+    email: 'leandro@email.com',
+    role: 'Admin',
+  };
+  userWage = 'R$5.000,00';
   title = 'cursoAngular';
 }
